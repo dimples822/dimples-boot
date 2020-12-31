fdf6a12cd1ad245f9efe1639bcc16f9afb41beb8
dimples <1126834403@qq.com>
2020-12-31 10:34:59 +0800

    新增StatusEnum

---
+++ dimples-core/src/main/java/com/dimples/core/eunm/StatusEnum.java
@@ -0,0 +1,60 @@
+package com.dimples.core.eunm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 状态
+ *
+ * @author zhongyj <1126834403@qq.com><br/>
+ * @date 2020/12/29
+ */
+public enum StatusEnum {
+
+    /**
+     * W
+     */
+    DeptCategory_ACTIVE("1", "启用"),
+    /**
+     * E
+     */
+    DeptCategory_DISABLE("0", "停用"),
+    /**
+     * DI
+     */
+    DeptCategory_DELETE("-1", "删除");
+
+    public static Map<String, String> map = new HashMap<>();
+
+    static {
+        StatusEnum[] values = StatusEnum.values();
+        if (values.length > 0) {
+            for (StatusEnum categoryEnum : values) {
+                map.put(categoryEnum.getStatusCode(), categoryEnum.getStatusDesc());
+            }
+        }
+    }
+
+    StatusEnum(String statusCode, String statusDesc) {
+        this.statusCode = statusCode;
+        this.statusDesc = statusDesc;
+    }
+
+    /**
+     * Code
+     */
+    @Setter
+    @Getter
+    private String statusCode;
+
+    /**
+     * Desc
+     */
+    @Setter
+    @Getter
+    private String statusDesc;
+
+}
